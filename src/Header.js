38382045a378827c8df923743d4ee0511c02986a
1916eco <1916eco@gmail.com>
2020-08-04 14:48:10 +0100

    init

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Home</h1>
+      <h1>Portfolio</h1>
+      <h1>About me</h1>
+      <h1>Pricing</h1>
+      <h1>Contact</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/temp.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2YHDht-K7rbTaNyS6k4pzD0MUhmcqpec",
+  authDomain: "photo-portfolio-813ac.firebaseapp.com",
+  databaseURL: "https://photo-portfolio-813ac.firebaseio.com",
+  projectId: "photo-portfolio-813ac",
+  storageBucket: "photo-portfolio-813ac.appspot.com",
+  messagingSenderId: "1013038737525",
+  appId: "1:1013038737525:web:60fa3c4e269e77ac4eb03c",
+  measurementId: "G-BTQR2V2GZY",
+};
